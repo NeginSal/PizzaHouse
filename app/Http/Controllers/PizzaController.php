f6306e5a769451444c2473e23ef6a997797b8a44
NeginSal <neginsaljooghi@gmail.com>
2022-05-11 17:18:31 +0430

    handling post request

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -21,21 +21,31 @@ class PizzaController extends Controller
     // $pizzas=Pizza::orderBy('name','desc')->get();
     // $pizzas = Pizza::where('type', 'hawaiian')->get();
     // $pizzas = Pizza::latest()->get();      
-
     $pizzas = Pizza::latest()->get();       
 
     return view('pizzas.index', [
       'pizzas' => $pizzas,
     ]);
   }
+
   public function show($id) {
     // use the $id variable to query the db for a record
     $pizza=Pizza::findOrFail($id);
 
     return view('pizzas.show', ['pizza' => $pizza]);
   }
+
   public function create() {
     return view('pizzas.create');
+  }
+
+  public function store(){
+
+    error_log(request('name')) ;
+    error_log(request('type')) ;
+    error_log(request('base')) ;
+    
+    return redirect('/');
   }
 
 }
