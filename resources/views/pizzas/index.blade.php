8f659cc8c13f3c0ed34bd6283849e9d6d784e47c
NeginSal <neginsaljooghi@gmail.com>
2022-05-17 16:05:12 +0430

    adding SASS

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -1,34 +1,15 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="title m-b-md">
-            Pizza List
-        </div>
- -->
-        
-        <!-- @for($i = 0; $i < 5; $i++)
-          <p>the value of i is {{ $i }}</p>
-        @endfor -->
-
-        <!-- @for($i = 0; $i < count($pizzas); $i++)
-          <p>{{ $pizzas[$i]['type'] }}</p>
-        @endfor -->
-
-        @foreach($pizzas as $pizza)
-          <div>
-            <!-- {{ $loop->index }} - {{ $pizza['type'] }} - {{ $pizza['base'] }}
-            @if($loop->first)
-              <span> - first in the loop</span>
-            @endif
-            @if($loop->last)
-              <span> - last in the loop</span>
-            @endif -->
-            
-          </div>
-        @endforeach
-
+<div class="wrapper pizza-index">
+  <h1>Pizza Orders</h1>
+  @foreach($pizzas as $pizza)
+    <div class="pizza-item">
+      <img src="/img/pizza.png" alt="pizza icon">
+      <h4><a href="/pizzas/{{ $pizza->id }}">{{ $pizza->name }}</a></h4>
     </div>
+  @endforeach
+
 </div>
+
 @endsection
