be9193425d3a46f08feb9c3d4b9916d9cb797683
NeginSal <neginsaljooghi@gmail.com>
2022-05-24 15:53:03 +0430

    Named Routes

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,11 @@ Route::get('/', function () {
 });
 
 
-Route::get('/pizzas','App\Http\Controllers\PizzaController@index')->middleware('auth');
-Route::get('/pizzas/create','App\Http\Controllers\PizzaController@create');
-Route::post('/pizzas','App\Http\Controllers\PizzaController@store');
-Route::get('/pizzas/{id}','App\Http\Controllers\PizzaController@show')->middleware('auth');
-Route::delete('/pizzas/{id}','App\Http\Controllers\PizzaController@destroy')->middleware('auth');
+Route::get('/pizzas','App\Http\Controllers\PizzaController@index')->name('pizzas.index')->middleware('auth');
+Route::get('/pizzas/create','App\Http\Controllers\PizzaController@create')->name('pizzas.create');
+Route::post('/pizzas','App\Http\Controllers\PizzaController@store')->name('pizzas.store');
+Route::get('/pizzas/{id}','App\Http\Controllers\PizzaController@show')->name('pizzas.show')->middleware('auth');
+Route::delete('/pizzas/{id}','App\Http\Controllers\PizzaController@destroy')->name('pizzas.destroy')->middleware('auth');
 
 
 
